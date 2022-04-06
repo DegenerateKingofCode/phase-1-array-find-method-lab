a433b3e54bddf302e1ee9a25e34185c94426db09
Tanner <Tanner@tleroy.com>
2022-04-06 18:53:49 -0400

    commit tree 7877cbeefe15a427b668e75680ea55b6279b67d2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+
+const superbowlWin = (record) => {
+    let finder = record.find(
+        (object) => { 
+            if (object.result === "W"){
+            return object.year 
+            }
+        }  
+    )
+    //debugger
+    if (finder === undefined){
+        return finder
+    } else {
+         return finder.year
+          }
+}
+ 
